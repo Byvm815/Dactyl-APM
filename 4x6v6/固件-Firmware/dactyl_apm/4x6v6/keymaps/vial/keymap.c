ee9f0a6e0ab08f8276fd4b5fe70c70427258d1bf
文青·白雨木 <99241417+Byvm815@users.noreply.github.com>
2023-10-20 21:33:17 +0800

    更改布局

--- 4x6v6/固件-Firmware/dactyl_apm/4x6v6/keymaps/vial/keymap.c
+++ 4x6v6/固件-Firmware/dactyl_apm/4x6v6/keymaps/vial/keymap.c
@@ -36,16 +36,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           
        C(KC_UP),KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           
         KC_NO,  KC_NO, KC_LBRC,KC_RBRC,                        
-                                MO(1),  MT(MOD_LGUI,KC_SPC),    
-                                KC_BSPC,KC_CAPS,                
+                                MT(MOD_LGUI,KC_SPC), MO(1),     
+                                KC_BSPC,KC_ESC,                
                                 KC_NO,  KC_NO,
 
         KC_Y,   KC_U,   KC_I,   KC_O,   KC_QUOT,KC_GRV, 
         KC_H,   KC_J,   KC_K,   KC_L,   KC_P,   KC_SCLN,
         KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_LCTL,
                         KC_MINS,KC_EQL, KC_NO,  KC_NO,                 
-        MT(MOD_LSFT,KC_ENT),  MO(2),                    
-KC_ESC, KC_DEL,                                 
+        MO(2),  MT(MOD_LSFT,KC_ENT),                  
+KC_CAPS,KC_DEL,                                 
 KC_NO , KC_NO                                                    
     ),
 
@@ -70,10 +70,10 @@ _______,_______
     [2] = LAYOUT(
         _______,_______,KC_HOME,KC_UP,  KC_END, KC_PGUP,                
         _______,_______,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,        
-        QK_BOOT,_______,_______,_______,_______,_______,        
+        _______,_______,_______,_______,_______,_______,         
         _______,_______,_______,_______,                           
                                         _______,KC_NO,             
-                                        _______,_______,           
+                                        _______,QK_BOOT,          
                                         _______,_______, 
 
         KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11, KC_F12,
